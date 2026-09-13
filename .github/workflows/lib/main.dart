4c70584943668ea014ab75aec5aa5030b84efc6f
XilianApp <290075359@qq.com>
2026-09-14 05:12:07 +0800

    Create main.dart for Flutter app structure
    Initial implementation of a Flutter app with a bottom navigation bar and three pages: Home, Device, and Settings.

---
+++ .github/workflows/lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'pages/home_page.dart';
+import 'pages/device_page.dart';
+import 'pages/settings_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '西连消防',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        useMaterial3: true,
+      ),
+      home: const MainPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _index = 0;
+
+  final _pages = const [
+    HomePage(),
+    DevicePage(),
+    SettingsPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_index],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _index,
+        onTap: (i) => setState(() => _index = i),
+        selectedItemColor: Colors.green[700],
+        unselectedItemColor: Colors.grey,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: '首页'),
+          BottomNavigationBarItem(icon: Icon(Icons.devices), label: '设备'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: '设置'),
+        ],
+      ),
+    );
+  }
+}
